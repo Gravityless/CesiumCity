233824e0f4520db74bf2b2427a4c6c796dfc0769
naichamilktea <2858759648@qq.com>
2022-11-11 01:34:17 +0800

    基本实现显示建筑功能

--- src/main/webapp/data/database/db_cesium.sql
+++ src/main/webapp/data/database/db_cesium.sql
@@ -2,7 +2,7 @@ drop table if exists tb_polygon;
 
 create table tb_polygon
 (
-    id int primary key auto_increment,
+    id int primary key ,
     polygon_name varchar(40),
     location varchar(40),
     margins varchar(800),
@@ -10,7 +10,7 @@ create table tb_polygon
     status int
 );
 
-insert into tb_polygon(polygon_name, location, margins, description, status)
-values ('polygon1', 'lontitude:xxx, latitude:xxx', '[{ x: -2733784.1592402603, y: 5093059.6151296245, z: 2686846.8706877907 }, { x: -2733781.032101405, y: 5093114.680154704, z: 2686746.3488108856 },{ x: -2733924.19395856, y: 5093041.994435746, z: 2686738.5149721224 },{ x: -2733881.373544848, y: 5093012.381819005, z: 2686837.5519579444 }]','Description...',0);
+insert into tb_polygon(id,polygon_name, location, margins, description, status)
+values (0,'polygon1', 'lontitude:xxx, latitude:xxx', '[{ x: -2733784.1592402603, y: 5093059.6151296245, z: 2686846.8706877907 }, { x: -2733781.032101405, y: 5093114.680154704, z: 2686746.3488108856 },{ x: -2733924.19395856, y: 5093041.994435746, z: 2686738.5149721224 },{ x: -2733881.373544848, y: 5093012.381819005, z: 2686837.5519579444 }]','Description...',0);
 
 SELECT *FROM tb_polygon;
--- src/main/webapp/js/showBuilding.js
+++ src/main/webapp/js/showBuilding.js
@@ -41,66 +41,108 @@ var showBuilding=function(){
         viewer.entities.remove(deleteobj)
         // Pick a new feature
         var pickedFeature = viewer.scene.pickPosition(movement.position);
+        var minx,miny,maxx,maxy,are,building_height,id;
         if(pickedFeature) {
             var cartographic = Cesium.Cartographic.fromCartesian(pickedFeature);
             var height_picked = cartographic.height; //高度
+            var pointx= Cesium.Math.toDegrees(cartographic.longitude);
+            var pointy= Cesium.Math.toDegrees(cartographic.latitude);
+            console.log(pointx,pointy);
+            var pol={};
+            var tableData;
+            axios({
+                method:"post",
+                url:"http://localhost:8080/CesiumCity/polygon/selectByPageAndCondition?currentPage="+ 0 +"&pageSize="+ 100,
+                data: pol,
+            }).then(function (resp) {
+                console.log(resp);
+                tableData = resp.data.rows;
+
+                //_this.totalCount = resp.data.totalCount;
+            }).then(function(){
+                if (tableData.length<98){
+                    return;
+                }
+                for (var i=0;i<98;++i){
+                    var str=tableData[i]['margins']
+                    str=str.split(',')
+                    if ((+pointx) >(+str[0]) && (+pointx) <(+str[1]) && (+pointy) >(+str[2]) && (+pointy) <(+str[3])){
+                        pointx=((+str[0])+(+str[1]))*0.5
+                        pointy=((+str[2])+(+str[3]))*0.5
+                        minx=+str[0]
+                        maxx=+str[1]
+                        miny=+str[2]
+                        maxy=+str[3]
+                        building_height=+tableData[i]['description']
+                        are=String(tableData[i]['location'])
+                        id=i
+                        console.log(i)
+                    }
+                }
+            }).then(function(){
+                clearEntity();
+            }).then(function (){
+                var real_height;
+                /*发送查询请求*/
+                /*接受请求，返回高度，面积，经纬度，名称,假设查询结果为经度属于[118.2202-118.2211],纬度属于[25.0748-25.0753]*/
+                building_pt1=[+minx,+miny]
+                building_pt2=[+minx,+maxy]
+                building_pt3=[+maxx,+miny]
+                building_pt4=[+maxx,+maxy]
+                building_center=[((+building_pt1[0])+(+building_pt2[0])+(+building_pt3[0])+(+building_pt4[0]))*0.25,((+building_pt1[1])+(+building_pt2[1])+(+building_pt3[1])+(+building_pt4[1]))*0.25]
+
+                console.log("building_center="+building_center);
+                let cartesians = [];let start = Cesium.Cartesian3.fromDegrees(building_center[0], building_center[1], 0);cartesians.push(start);viewer.scene
+                    .clampToHeightMostDetailed(cartesians)
+                    .then(function (clampedCartesians) {
+                        var cartographic = Cesium.Cartographic.fromCartesian(clampedCartesians[0]);
+                        var height=cartographic.height;
+                        document.getElementById("tmp").innerHTML=String(height);
+                        building_height=document.getElementById("tmp").innerHTML;
+                        //document.write(building_height);
+                        real_height=(+building_height)-42.25;
+                        viewer.entities.add({
+                            id:'box',
+                            position:Cesium.Cartesian3.fromDegrees(building_center[0], building_center[1], 42.25+0.5*real_height),
+                            box:{
+                                dimensions : new Cesium.Cartesian3(0.0006*111000, 0.0003*111000*Math.cos(23/180*3.1415926), real_height),
+                                material : Cesium.Color.RED.withAlpha(0.5),
+                            }
+                        })
+
+                    });
+            }).then(function (){
+                selectedEntity.name = 'test';
+                selectedEntity.description =
+                    'Loading <div class="cesium-infoBox-loading"></div>';
+                viewer.selectedEntity = selectedEntity;
+                selectedEntity.description =
+                    `${
+                        '<table class="cesium-infoBox-defaultTable"><tbody>' +
+                        "<tr><th>name</th><td>"
+                    }${/*pickedFeature.getProperty("BIN")*/("Building"+String(id))}</td></tr>` +
+                    `<tr><th>height</th><td>${/*pickedFeature.getProperty(
+            "DOITT_ID"
+        )*/height_picked}</td></tr>` +
+                    `<tr><th>lon/lat</th><td>${/*pickedFeature.getProperty(
+            "SOURCE_ID"
+        )*/(""+pointx+",    "+pointy)}</td></tr>` + `<tr><th>area</th><td>${/*pickedFeature.getProperty(
+            "SOURCE_ID"
+        )*/are}</td></tr>`;
+            })
         }
         if (!Cesium.defined(pickedFeature)) {
         clickHandler(movement);
         return;
         }
-        var building_height,real_height;
-        /*发送查询请求*/
-        /*接受请求，返回高度，面积，经纬度，名称,假设查询结果为经度属于[118.2202-118.2211],纬度属于[25.0748-25.0753]*/
-        building_pt1=[118.2202,25.0748]
-        building_pt2=[118.2202,25.0753]
-        building_pt3=[118.2211,25.0748]
-        building_pt4=[118.2211,25.0753]
-        building_center=[((+building_pt1[0])+(+building_pt2[0])+(+building_pt3[0])+(+building_pt4[0]))*0.25,((+building_pt1[1])+(+building_pt2[1])+(+building_pt3[1])+(+building_pt4[1]))*0.25]
 
-
-        let cartesians = [];let start = Cesium.Cartesian3.fromDegrees(building_center[0], building_center[1], 0);cartesians.push(start);viewer.scene
-                .clampToHeightMostDetailed(cartesians)
-                .then(function (clampedCartesians) {
-                    var cartographic = Cesium.Cartographic.fromCartesian(clampedCartesians[0]);
-                    var height=cartographic.height;
-                    document.getElementById("tmp").innerHTML=String(height);
-                    building_height=document.getElementById("tmp").innerHTML;
-                    //document.write(building_height);
-                    real_height=(+building_height)-42.25;
-                    viewer.entities.add({
-                        id:'box',
-                        position:Cesium.Cartesian3.fromDegrees(building_center[0], building_center[1], 42.25+0.5*real_height),
-                        box:{
-                            dimensions : new Cesium.Cartesian3(0.0009*111000, 0.0005*111000*Math.cos(23/180*3.1415926), real_height),
-                            material : Cesium.Color.RED.withAlpha(0.5),
-                        }
-                    })
-
-                });
 
 
         //console.log(building_height);
         /*console.log("centerheight:");console.log(center_height);
         console.log("edgeheight:");console.log(edge_height);*/
 
-        selectedEntity.name = 'test';
-        selectedEntity.description =
-        'Loading <div class="cesium-infoBox-loading"></div>';
-        viewer.selectedEntity = selectedEntity;
-        selectedEntity.description =
-        `${
-            '<table class="cesium-infoBox-defaultTable"><tbody>' +
-            "<tr><th>name</th><td>"
-        }${/*pickedFeature.getProperty("BIN")*/"name"}</td></tr>` +
-        `<tr><th>height</th><td>${/*pickedFeature.getProperty(
-            "DOITT_ID"
-        )*/"height"}</td></tr>` +
-        `<tr><th>lon/lat</th><td>${/*pickedFeature.getProperty(
-            "SOURCE_ID"
-        )*/"lon/lat"}</td></tr>` + `<tr><th>area</th><td>${/*pickedFeature.getProperty(
-            "SOURCE_ID"
-        )*/"area"}</td></tr>`;
+
     },
     Cesium.ScreenSpaceEventType.LEFT_CLICK);
     
